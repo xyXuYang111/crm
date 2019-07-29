762a87ba22a0c5d159fd4574e811e3525ba8a793
xuy <15172399690>
2019-07-29 08:31:26 +0800

    整理

---
+++ src/main/java/com/xuyang/crm/mongo/repsitory/MongoRepository.java
@@ -0,0 +1,14 @@
+package com.xuyang.crm.mongo.repsitory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: cypc
+ * @Date: 2019-7-29 08:28
+ * @Description:
+ */
+@Slf4j
+@Component
+public class MongoRepository {
+}
